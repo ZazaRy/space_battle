84073d5b58cffff65193be7a210bf998b75ed8f9
Valentin Rafael <rylikanalysis@gmail.com>
2024-09-29 15:57:21 +0100

    finished core mechanics

--- main.py
+++ main.py
@@ -1,25 +1,76 @@
 import pygame
+import sys
 from constants import *
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     print("Starting asteroids!")
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+
     while True:
-        screen.fill("BLACK")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
 
+        for obj in asteroids:
+            if player.check_colls(obj):
+                print("Game Over")
+                return
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.check_colls(player):
+                print("Game Over")
+                sys.exit()
+
+            for shot in shots:
+                if asteroid.check_colls(shot):
+                    asteroid.split()
+                    shot.kill()
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 
 if __name__ == "__main__":
     main()
+
 
